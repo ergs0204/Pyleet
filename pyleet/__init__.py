37318acd853ddace6e5bd3b4cb4516ddad74cc32
ergs0204 <69901697+ergs0204@users.noreply.github.com>
2025-06-19 18:34:06 +0800

    Finish fetching testcase via `pyleet.get_testcase`

--- pyleet/__init__.py
+++ pyleet/__init__.py
@@ -8,5 +8,8 @@ from .common import ListNode, TreeNode
 # Programmatic interface
 from .programmatic import run, print_results
 
+# Test case retrieval
+from .testcase_retriever import get_testcase
+
 __all__ = ['register_deserializer', 'register_serializer',
-           'ListNode', 'TreeNode', 'run', 'print_results']
+           'ListNode', 'TreeNode', 'run', 'print_results', 'get_testcase']
